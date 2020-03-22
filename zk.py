2f17ff7e54b79f6adcbe14ded4c650565f175b46
Jasse Wistrand <jasse.wistrand@iki.fi>
2020-03-22 20:09:38 +0100

    Refactor the database

--- zk.py
+++ zk.py
@@ -14,79 +14,12 @@ from typing import *
 import note_folder as NF
 from note_folder import NoteFiles
 from note_folder import NotesDirectory
+from note_database import NoteDatabase
 import daily
 
 log = logging.getLogger(__name__)
 ZK_DIR = None
 ZK_DB = None
-
-
-class NoteDatabase:
-    IS_MAJOR_NUMBER = re.compile('^[0-9]+$')
-    IS_ANY_NOTE = re.compile('^[0-9]+|[0-9]+[a-z]|[0-9]+([a-z][0-9]+)+')
-
-    def __init__(self, sqlite_connection: sqlite3.Connection):
-        """ Start using fully initialized database """
-        self._database_handle = sqlite_connection
-
-    def commit(self):
-        self._database_handle.commit()
-
-    def find_major_notes(self) -> Set[str]:
-        cursor = self._database_handle.cursor()
-        cursor.execute('select name from notes')
-        notes_in_database = cursor.fetchall()
-        cursor.close()
-
-        major_notes = set()
-        for note in notes_in_database:
-            note = note[0]
-            if NoteDatabase.IS_MAJOR_NUMBER.match(note):
-                major_notes.add(note)
-        return major_notes
-
-    def find_all_notes(self) -> Set[str]:
-        cursor = self._database_handle.cursor()
-        cursor.execute('select name from notes')
-        notes_in_database = cursor.fetchall()
-        cursor.close()
-
-        major_notes = set()
-        for note in notes_in_database:
-            note = note[0]
-            if NoteDatabase.IS_ANY_NOTE.match(note):
-                major_notes.add(note)
-        return major_notes
-
-
-    def save_card(self, card_name: str, card_path: str):
-        notes = find_open_notes()
-        if not os.path.isfile(card_path):
-            raise EnvironmentError(f'Missing card: {card_path}')
-
-        cursor = self._database_handle.cursor()
-        stat = os.stat(card_path)
-        created_utc  = int(stat.st_ctime)
-        modified_utc = int(stat.st_mtime)
-        with open(card_path, 'rb') as fd:
-            content = fd.read()
-            sql1 = ('insert into notes(name, content, created_utc, modified_utc) values (?, ?, ?, ?)')
-            sql2 = ('on conflict(name) do update set content=excluded.content, modified_utc=excluded.modified_utc')
-            args = (card_name, content, created_utc, modified_utc)
-
-            cursor.execute(' '.join((sql1, sql2)), args)
-
-        self.commit()
-
-    def card_modified_utc_time_in_seconds(self, card_name: str) -> Optional[int]:
-        cursor = self._database_handle.cursor()
-        cursor.execute('select modified_utc from notes where name = ?', (card_name,))
-        modified_time = cursor.fetchone()
-        cursor.close()
-
-        if not modified_time:
-            return None
-        return int(modified_time[0])
 
 
 class Notes:
@@ -400,6 +333,7 @@ if __name__ == '__main__':
             content = date.strftime('%F Daily\n\n\n')
             card_path = create_new_open_card(content=content, app=notes)
             card_name = os.path.basename(card_path)
+            notes.persistent_notes.save(card_name, card_path)
             daily.set_the_daily_card(card_name, date, notes.database_handle)
         else:
             card_path = notes.open_notes.fullpath_of_open_card(card_name)
